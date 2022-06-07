36b105432f5e5392663913cef4005d2b6ce5ee93
RalphRebello <ralph.rebello8@gmail.com>
2022-06-06 22:38:42 -0300

    Dicionarios

---
+++ Session7/Dictionaries.py
@@ -0,0 +1,12 @@
+"""
+OBS: Em algumas linguagens os dicionarios sao conhecidos por mapas
+Dicionarios sao coleçoes de tipo chave:valor
+"""
+
+countries = {
+    'br': 'Brasil',
+    'eua': 'Estados Unidos',
+    'uy': 'Uruguai'
+}
+
+print(countries.values())
